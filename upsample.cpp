3f83f359b0aadc70904e13ea6fa7b1cfbe6ff7a0
Carlos Alfonso <caa16f@my.fsu.edu>
2020-11-16 00:30:53 -0500

    Main Project

--- upsample.cpp
+++ upsample.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
+
 #include <sstream>
 #include <vector>
 #include <string>
+#include <cmath>
 
  std::vector<int> nnInterpolation(std::vector<int> data , int rowNums, int colNums, int newRowNums, int newColNums){
     std::vector<int> temp(newRowNums*newColNums );
 
-    int x_ratio = (int) ((rowNums<<16)/newRowNums) +1 ;
+    int x_ratio = (int) ((rowNums << 16) / newRowNums) +1 ;
     int y_ratio = (int) ((colNums << 16) / newColNums) +1;
 
     int newX, newY;
@@ -23,47 +25,73 @@
     return temp;
 }
 
+int l1Distance(std::vector<int> original , std::vector<int> modified  ){
+
+    int distance = 0;
+    for(int i = 0; i < 512*512; i++){
+            distance += std::abs((original[i] - modified[i]));
+    }
+    return distance;
+}
+
 
 int main(int argc , char **argv) {
 
     std::ifstream f;
-    std::string line, val;
     std::vector<int> holder;              //MAIN ARRAY HOLDING DATA
 
-    if( argc > 1){
-        f.open(argv[1]);
-        if(!f.is_open()){
-            std::cerr << "Error , file opening failed :" << argv[1] << ".\n";
-            return 1;
+    //Read in csv to holder
+    int x;
+    char trash;
+    for(int i = 0 ; i < 256; ++i) {
+        for(int j = 0; i < 256*256; ++i) {
+            std::cin >> x;
+            holder.push_back(x);
+            if(i < (256*256 -1))
+                std::cin >> trash;
         }
-    }else{
-        std::cerr << "Missing file input: File required for program to run";
-        return 1;
     }
 
-
-    while(std::getline(f,line)) {
-        std::vector<int> v;         //Vector to hold the rows
-        std::stringstream s(line);      //String stream s
-        while (getline(s, val, ','))
-            holder.push_back(std::stoi(val));
-       // holder.push_back(v);
-    }
-
-
-    for (auto& row : holder) {
-       //     std::cout << row << "  ";
-    }
-    std::cout << holder.size() << std::endl;
-
+    int testOutput[512][512];
+    int arrOutput[512][512];
 
     std::vector<int> outPut = nnInterpolation(holder,256, 256, 512, 512);
 
 
+    std::ifstream f2 ("sample1_original_image.txt");
 
+    std::vector<int> test;              //MAIN ARRAY CONFIRMED DATA
+    std::string line2;
+    while(getline(f2,line2)){
+        std::istringstream iss(line2);
+        std::string holder;
+        while(getline(iss,holder,',')){
+            test.push_back(std::stoi(holder));
+        }
+    }
+    for(int i = 0 ; i < test.size() ; i++){
+        std::cout << test[i] << " ";
+    }
+
+
+    std::cout << holder.size() << std::endl;
     std::cout << outPut.size() << std::endl;
 
+    std::cout << l1Distance(test, outPut) << std::endl;
 
+
+    std::ofstream outputFile;
+
+    outputFile.open("output.txt");
+    outputFile << "[ ";
+    for(int i = 0; i < 512*512; i++){
+       if(i % 512 == 0 && i != 0){
+           outputFile << "\n";
+       }
+        outputFile << outPut[i] << ", ";
+    }
+    outputFile << " ]";
+    outputFile.close();
     return 0;
 }
 
